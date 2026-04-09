e155ac39e80c224414db6adf713f4a25ab793db4
AnushkaKakade <anushkakakade802@gmail.com>
2026-04-09 08:39:48 +0000

    removed unwanted files

--- admin/manage_medicine.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("../config/db.php");
-
-$result = $conn->query("SELECT * FROM medicines");
-
-while($row = $result->fetch_assoc()){
-    echo $row['name']." - ".$row['quantity']."<br>";
-}
-?>
--- pharmacist/billing.php
+++
@@ -1,8 +0,0 @@
-<?php
-$result = $conn->query("SELECT * FROM medicines 
-WHERE quantity < 10 OR expiry_date < CURDATE() + INTERVAL 7 DAY");
-
-while($row = $result->fetch_assoc()){
-    echo "Alert: ".$row['name']."<br>";
-}
-?>
